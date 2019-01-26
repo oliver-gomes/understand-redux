3951e77e591663a7f74160928b8a9b4017768c1a
Oliver Gomes <ogomesphoto@gmail.com>
2019-01-25 23:00:18 -0500

    implement redux to carry initialState

--- user-card/src/App.js
+++ user-card/src/App.js
@@ -1,39 +1,42 @@
 import React, { Component } from "react";
-import user from "./images/user.png";
+// import user from "./images/user.png";
+import avatar from "./images/man.png";
 import "./App.css";
 
+import reducers from "./reducers";
+import { createStore } from "redux";
+
+const initialState = {
+  name: "Alex Bakery",
+  description: "Software Engineer, Speaker, and Occasional Model",
+  likes: "Cats, Wine, and Black dresses",
+  location: "localhost"
+};
+const store = createStore(reducers, initialState);
+
 class App extends Component {
-  state = {
-    name: "Alex Bakery",
-    description: "Software Engineer, Speaker, and Occasional Model",
-    likes: "Cats, Wine, and Black dresses",
-    location: "localhost"
-  };
   render() {
-    const { name, description, likes, location } = this.state;
     return (
       <div className="App">
         <section className="User__img">
-          <img src={user} alt="user" />
+          <img src={avatar} alt="user" />
         </section>
 
         <section className="User__info">
           <p>
-            {" "}
-            <span className="faint">I am</span> a {description}
+            <span className="faint">I am</span> a {store.getState().description}
           </p>
           <p>
-            {" "}
-            <span className="faint">I like</span> {likes}
+            <span className="faint">I like</span> {store.getState().likes}
           </p>
 
           <p className="User__info__details User__info__divider faint">
             <span>Name: </span>
-            <span>{name}</span>
+            <span>{store.getState().name}</span>
           </p>
           <p className="User__info__details faint">
             <span>Location: </span>
-            <span>{location}</span>
+            <span>{store.getState().location}</span>
           </p>
         </section>
       </div>

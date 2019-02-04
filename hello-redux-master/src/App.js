1810434b62db029455a01778a8c3bd374cc5777b
Oliver Gomes <ogomesphoto@gmail.com>
2019-02-03 19:57:44 -0500

    refactored to use react-redux

--- hello-redux-master/src/App.js
+++ hello-redux-master/src/App.js
@@ -1,15 +1,24 @@
 import React, { Component } from "react";
 import HelloWorld from "./HelloWorld";
 import ButtonGroup from "./ButtonGroup";
-import { store } from "./store";
+import { connect } from "react-redux";
 
 class App extends Component {
   render() {
     return [
-      <HelloWorld key={1} tech={store.getState().tech} />,
+      <HelloWorld key={1} tech={this.props.tech} />,
       <ButtonGroup key={2} technologies={["React", "Elm", "React-redux"]} />
     ];
   }
 }
 
-export default App;
+function mapStateToProps(state) {
+  return {
+    tech: state.tech
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  null
+)(App);
--- hello-redux-master/src/ButtonGroup.js
+++ hello-redux-master/src/ButtonGroup.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { store } from "./store";
 import { setTechnology } from "./actions";
+import { connect } from "react-redux";
 
 function dispatchBtnAction(e) {
   const tech = e.target.dataset.tech;
